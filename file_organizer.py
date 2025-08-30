11e0ebb98776749235ca03911a5fcf170bb83971
Inferno <ishaansingh0306@gmail.com>
2025-08-30 14:09:56 +0530

    file_organizer.py

--- file_organizer.py
+++ file_organizer.py
@@ -15,7 +15,7 @@ FILE_TYPES = {
     "Others": []  # everything else
 }
 
-def organize_folder(folder_path):
+def organize_folder(folderpath):
     # Loop through all files in the folder
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
